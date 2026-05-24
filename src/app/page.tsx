0cab7688d407b788fcd0e76dae635d4ad5759a26
Ihya' Ulumuddin <pebehakun45@gmail.com>
2026-05-24 16:16:48 +0700

    chore(home-page): remove comment code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 import { HomeClient } from "@/app/home-client";
-// import { getBetterAuthSession } from "@/proxy";
 
 export default async function HomePage() {
-  // const session = await getBetterAuthSession();
-
   return <HomeClient />;
 }
